cdeff4135623a62da4071209bbb68020492b0034
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-03-28 12:01:59 +0200

    Update fuzzWalletTest.sol

--- test/store/wallet/fuzzWalletTest.sol
+++ test/store/wallet/fuzzWalletTest.sol
@@ -1,68 +1,76 @@
-SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.15;
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.24;
 
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
 
-import "../../src/Wallet/Wallet.sol";
+import "../../src/wallet/CollectorsWallet.sol";
 
-contract FuzzWalletTest is Test{
+contract collectorsFuzzTest is Test {
+    CollectorsWallet public walletGabaim;
+    address userAddress = vm.addr(1);
 
-    Wallet public W;
-    address randomUser = vm.addr(1234); // create random user
-    function  setUp() public{
-        W = new Wallet();
+    function setUp() public {
+        walletGabaim = new CollectorsWallet();
+        // why it is not work?
+        // payable(address(walletGabaim)).transfer(1000); // move 1000 to the contract
     }
 
-       function testReceive(uint256 amount) public {
-        uint256 balance = wallet.balance();
-        vm.startPrank(randomUser); // send from random address
-        vm.deal(randomUser, amount); // put money in this wallet
-        payable(address(W)).transfer(amount); // move amount to the contract
-        assertEq(wallet.balance(), balance+ amount);
-        vm.stopPrank();
-        
-    }
-    function testAllowedWithdraw(uint256 amount) external {
-        vm.deal(randomUser, amount);
-        vm.startPrank(randomUser); // send from random address
-        console.log(amount);
-        payable(address(w)).transfer(amount);
-        uint256 balance = wallet.balance();
-        W.withdraw(amount);
-        assertEq(wallet.balance(), initialBalance - amount);
+    function testReceive(uint256 amount) public {
+        console.log(amount, " receive amount");
+        vm.startPrank(userAddress);
+        vm.deal(userAddress, amount);
+        uint256 initialBalance = address(walletGabaim).balance; // the balance in the begining (before transfer)
+        payable(address(walletGabaim)).transfer(amount); // move amount to the contract
+        uint256 finalBalance = address(walletGabaim).balance; // the balance in the final (aftere transfer)
+        assertEq(finalBalance, initialBalance + amount);
         vm.stopPrank();
     }
 
-    function testNotAllowedWithdraw(uint256 amount) external {
-        address userAddress = vm.addr(12); // address of not allowed user
-        vm.startPrank(userAddress); // send from random address
-        console.log(amount);
+    function testAllowedWithDraw(uint256 amount) external {
+        console.log(amount, " with draw amount");
+        userAddress = 0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f; // address of allowed user
+        vm.startPrank(userAddress); // send from user address
+        vm.deal(userAddress, amount); 
+        payable(address(walletGabaim)).transfer(amount); // move amount to the contract
+        uint256 initialBalance = address(walletGabaim).balance; // the balance in the begining (before transfer)
+        walletGabaim.withdraw(amount);
+        uint256 finalBalance = address(walletGabaim).balance; // the balance in the final (after transfer)
+        assertEq(finalBalance, initialBalance - amount);
+        vm.stopPrank();
+    }
+
+    function testWithDrawNoMoney(uint256 amount) external {
+        console.log(amount, " with draw amount");
+        userAddress = 0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f; // address of allowed user
+        vm.startPrank(userAddress); // send from user address
+        if(amount!= 0){
+            vm.expectRevert();
+        }
+        walletGabaim.withdraw(amount);
+        vm.stopPrank();
+    }
+
+    function testNotAllowedWithDraw(uint256 amount) external {
+        console.log(amount, " with draw amount");
+        // console.log(userAddress, " with draw randomAddress");
+        vm.startPrank(userAddress); // send from user address
+        vm.deal(userAddress, amount); 
+        payable(address(walletGabaim)).transfer(amount); // move amount to the contract
         vm.expectRevert();
-        W.withdraw(amount);
-     }
-
-    function testUpdate(address oldGabai, address newGabai ) public{
-       
-        //address userAddress = vm.addr(1); // address of not allowed user
-        vm.startPrank(userAddress); // send from random address
-        if(oldGabai=newGabai){
-            vm.expectRevert("the new gabai already gabai");
-            W.update(oldGabai, newGabai);
-        }
-        else{
-            W.update(oldGabai, newGabai);
-            assertEq(W.gabaim(newGabai),1);
-            assertEq(W.gabaim(oldGabai),0);
-        }
+        walletGabaim.withdraw(amount);
         vm.stopPrank();
-
-    }
-    function testGetBalance() public{
-       
-        // assertEq(wallet.getBalance(), 50 , "not equals"); 
-        assertEq(W.getBalance(), address(W).balance , "not equals");
     }
 
+    function testUpdateCollectors(address oldGabai, address newGabai) public {
+        address ownerAddress = 0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496; // address of owner user
+        vm.startPrank(ownerAddress); // send from owner address
+        console.log(address(msg.sender));
+        if(walletGabaim.collectors(oldGabai)!=1){
+            vm.expectRevert();
+        }
+        walletGabaim.updateCollectors(oldGabai, newGabai);
+        vm.stopPrank();
+    }
 }
