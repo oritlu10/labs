e65358d0436c3369255467d0983bb40d4a90afa6
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-04-09 09:20:07 +0300

    Update statkingTest.sol

--- test/store/statking/statking1.sol
+++ test/store/statking/statking1.sol
@@ -1,34 +1,30 @@
-// SPDX-License-Identifier: private
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
 
-import "@hack/like/IERC20.sol";
-import "@hack/erc20/erc20.sol";
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
 
-contract TestChange in test {
-    Change c;
-    ERC20 public usdc;
-    ERC20 public usdt;
-    constructor() {
-        c = new Change();
-        usdc = ERC20(c.usdc());
-        usdt = ERC20(c.usdt());
+import "../../src/Staking/Staking.sol";
+
+contract StakingTest is Test{
+
+    Staking public staking;
+    function setUp() public{
+        staking = new Staking();
+
     }
-    function testGetSymbols() public {
-        (string memory symbol1, string memory symbol2) = c.getSymbols();
-        Assert.equal(symbol1, "myUSDC", "Symbol 1 should be myUSDC");
-        Assert.equal(symbol2, "myUSDT", "Symbol 2 should be myUSDT");
+    function testDeposit() public{
+        
+        uint256 initialPoolBalance = staking.poolBalance();
+        console.log(initialPoolBalance);
+        staking.deposit();
+        uint256 finalPoolBalance = staking.poolBalance();
+        console.log(finalPoolBalance);
+        
+
     }
-    function testChange() public {
-        uint256 amount = 100;
-        // Transfer tokens to the Change contract
-        usdc.transfer(address(c), amount);
-        usdc.approve(address(c), amount);
-        // Perform token change
-        c.change(amount);
-        // Check balances after change
-        uint256 usdcBalance = usdc.balanceOf(address(this));
-        uint256 usdtBalance = usdt.balanceOf(address(this));
-        Assert.equal(usdcBalance, amount, "USDC balance should be correct");
-        Assert.equal(usdtBalance, amount, "USDT balance should be correct");
+    function testWithdraw() public{
+
     }
 }
