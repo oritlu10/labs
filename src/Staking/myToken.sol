bc942d2255947c29ac7b3c78fc2d431a1de6c010
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-05-01 11:35:37 +0300

    Update myToken.sol

--- src/Staking/myToken.sol
+++ src/Staking/myToken.sol
@@ -1,1 +1,8 @@
-
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.24;
+import "../audit/approve.sol";
+contract MyToken is ERC20 {
+    constructor() ERC20()  {}
+   
+}
