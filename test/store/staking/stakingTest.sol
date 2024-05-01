9097aace9084dfd3f7fe6dcb00ffe579638d453e
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-05-01 10:48:00 +0300

    Update stakingTest.sol

--- test/store/staking/stakingTest.sol
+++ test/store/staking/stakingTest.sol
@@ -1,59 +1,45 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.24;
 
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "../../new_project/src/MyToken.sol";
+import "../src/Staking/Staking.sol";
+import "../src/audit/approve.sol";
+contract StakingTest is Test {
+    Staking public staking;
+    MyToken public myToken;
 
-import "../../src/Staking/Staking.sol";
-
-contract StakingTest is Test{
-
-    Staking  public staking;
-    MyToken token;
 
     function setUp() public {
-        token = new MyToken();
-        staking = new Staking(token);
+        myToken = new MyToken();
+        staking = new Staking();
+        myToken.mint(10**6);
     }
 
     function testDeposit() public {
-        uint256 amount = 1000; 
-        token.mint(msg.sender, amount);
-        token.approve(address(staking), amount);
-        staking.deposit(amount);
-        assertEq(staking.poolBalance(), amount, "Pool balance should match deposited amount");
+        uint256 initialPoolBalance = staking.stakingPool();
+        staking.deposit(1000);
+        uint256 finalPoolBalance = staking.stakingPool();
+        assertEq(initialPoolBalance + 1000, finalPoolBalance);
     }
 
-    function testWithdraw() public {
-        uint256 initialBalance = 1000; 
-        uint256 withdrawAmount = 500; 
-        token.mint(address(staking), initialBalance);
-        staking.deposit(initialBalance);
-        uint256 initialUserBalance = token.balanceOf(msg.sender);
-        staking.withdraw(withdrawAmount);
-        uint256 finalUserBalance = token.balanceOf(msg.sender);
-        assertEq(finalUserBalance - initialUserBalance, withdrawAmount, "User balance should increase by withdraw amount");
-        assertEq(staking.poolBalance(), initialBalance - withdrawAmount, "Pool balance should decrease by withdraw amount");
-    }
-
-    function testCalculateDays() public {
-        uint256 amount = 1000;
-        token.mint(address(this), amount);
-        token.approve(address(staking), amount);
-        staking.deposit(amount);
-        assertEq(staking.database(msg.sender).length, 1, "There should be one entry in the user's database");
-     //   uint256 expectedSum = amount * 7;
-        AssertEq(staking.calculateDays(amount), amount, "Calculated sum should match expected sum");
-    }
-
-    function testCalculateSum() public {
-        uint256 sum = 1000;
-        uint256 rate = 2;
-        uint256 poolBalance = 5000;
-        uint256 expectedBonus = (sum * rate * 1000000000) / poolBalance;
-        assertEq(staking.calculateSum(sum) expectedBonus, "Calculated bonus should match expected bonus");
+    function testAllowWithdraw() public {
+        address userAddress = vm.addr(123);
+        vm.startPrank(userAddress);
+        vm.deal(userAddress, 500);
+        uint256 dateBefore7Days = block.timestamp - 604800;
+        uint256 sum=500;
+        staking.pushDatabase(dateBefore7Days,sum,userAddress);
+        uint256 initialPoolBalance = staking.stakingPool();
+        uint256 initalUserBalance = staking.myToken().balanceOf(userAddress);
+        staking.print(userAddress);
+        staking.withdraw(500);
+        staking.print(userAddress);
+        uint256 finalPoolBalance = staking.stakingPool();
+        assertEq(initialPoolBalance - 500, finalPoolBalance);
+        uint256 finalUserBalance = staking.myToken().balanceOf(userAddress);
+        assertEq(initalUserBalance - 500, finalPoolBalance - staking.calculateSum(500));  
     }
 }
 
