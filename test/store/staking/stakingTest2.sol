c1a5382e37b6abfbd5e01aed660b1a303d30c495
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-05-01 15:29:22 +0300

    Create stakingTest2.sol

---
+++ test/store/staking/stakingTest2.sol
@@ -0,0 +1,11 @@
+ function withdraw( uint256 amount) external updateReward(msg.sender) {
+        uint256 amount = 1000;
+        require(amount > 0, "amount = 0");
+        intinalBalance = balances[msg.sender]
+        balances[msg.sender] -= amount;
+        beforeTransfer = staked
+        staked -= amount;
+        stakingToken.transfer(msg.sender, amount);
+        assertEq( intinalBalance, intinalBalance+1000 ,"not equal")
+        assertEq( beforeTransfer, intinalBalance+1000 ,"not equal")
+    }
