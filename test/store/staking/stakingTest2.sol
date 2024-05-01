af40be428024775fd5680d57df6700e0ca8c3efa
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-05-01 15:30:06 +0300

    Update stakingTest2.sol

--- test/store/staking/stakingTest2.sol
+++ test/store/staking/stakingTest2.sol
@@ -1,4 +1,16 @@
- function withdraw( uint256 amount) external updateReward(msg.sender) {
+pragma solidity ^0.8.24;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../src/Staking/Staking.sol";
+import "../src/audit/approve.sol";
+contract StakingTest is Test {
+
+
+
+
+function withdraw( uint256 amount) external updateReward(msg.sender) {
         uint256 amount = 1000;
         require(amount > 0, "amount = 0");
         intinalBalance = balances[msg.sender]
