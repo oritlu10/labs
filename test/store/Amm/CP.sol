f7164dcd1da191634bb2cc2a2abf40650f360104
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-05-07 11:58:55 +0300

    Update CP.sol

--- test/store/Amm/CP.sol
+++ test/store/Amm/CP.sol
@@ -9,9 +9,17 @@ contract CP {
        uint public reserve0;
        uint public reserve1;
 
-       mapping(address => uint public balances;
+       mapping(address => uint) public balances;
 
        constractor(address t0, address t1) {
           token0 = IERC20(t0);
           token1 = IERC20(t1);
        }
+
+       function addLiquidity(uint amount0, uint amount1) external returns (uint shares) {
+           token0.transferFrom(msg.sender, address(this), amount0);
+           token1.transferFrom(msg.sender, address(this), amount1);
+           if (resreve0 > 0 || resreve1 > 0 ) {
+               require(reserve0* amount1 == reserve1 amount0, "x/y != dx/dy");
+
+
