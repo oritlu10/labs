f7ca69e0cf17141822dd6b2de78dc1020c6338ac
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-05-08 16:38:14 +0300

    Update splitTest.sol

--- test/store/split/splitTest.sol
+++ test/store/split/splitTest.sol
@@ -1,1 +1,21 @@
+SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.20;import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "/home/sarih/labs/task/cp.sol"
 
+contract testSplit in Test {
+
+    testSplit ts;
+
+    constructor(){
+     ts = new testSplit;
+    }
+    function testSplitMoney() public {
+         uint amount=80
+         ts.splitMoney(amoumt);
+         uint amountPerBeneficiary = amount / addressArray.length;
+         assert.Eq(ts.balanceOf(addressArray[i]), amountPerBeneficiary, "Amount was not distributed equally");
+    }
+
+}
