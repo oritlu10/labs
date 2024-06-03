17964a5d69c11def5bf9ba4337620d371e0d9c02
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-06-03 16:49:41 +0300

    Create testSlot.sol

---
+++ src/Proxy/testSlot.sol
@@ -0,0 +1,13 @@
+contract TestSlot {
+    bytes32 public constant slot = keccak256("TEST_SLOT");
+
+    function getSlot() external view returns (address) {
+        return StorageSlot.getAddressSlot(slot).value;
+    }
+
+    function writeSlot(address _addr) external {
+        StorageSlot.getAddressSlot(slot).value = _addr;
+    }
+
+   
+}
