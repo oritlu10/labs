b13fde82a2403673098731fdd6927b0f164570fa
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-06-03 15:12:42 +0300

    Update aDai.sol

--- src/Lending/aDai.sol
+++ src/Lending/aDai.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import "@openzeppelin/ERC20/IERC20.sol";
+import "@openzeppelin/ERC20/extensions/ERC20Permit.sol";
+
+contract Token1 is ERC20 {
+    constructor() ERC20("aDai", "aD") {}
+
+    function mint(address add, uint256 amount) public {
+        _mint(add, amount);
+    }
+}
