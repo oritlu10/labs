f000b354a4e09bd107ecf2d67326ea79f46f675a
oritGreenvald <121660885+oritlu10@users.noreply.github.com>
2024-06-03 15:13:39 +0300

    Create dai.sol

---
+++ src/Lending/dai.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/ERC20/IERC20.sol";
+import "@openzeppelin/ERC20/extensions/ERC20Permit.sol";
+
+contract Token1 is ERC20 {
+    constructor() ERC20("dai", "D") {}
+
+    function mint(address add, uint256 amount) public {
+        _mint(add, amount);
+    }
+}
